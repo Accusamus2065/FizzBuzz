7244322aa537821da70844cee93b0cc7a80df3f9
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2023-09-11 16:50:28 -0400

    some little changes

--- src/Reduce.java
+++ src/Reduce.java
@@ -7,11 +7,7 @@ public class Reduce {
     private static int doReduce(int n) {
         int count = 0;
         while (n != 0) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n -= 1;
-            }
+            n = n % 2 == 0 ? n / 2 : n - 1;
             count++;
         }
         return count;
